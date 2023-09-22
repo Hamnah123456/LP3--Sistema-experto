acad572b20e5b5df9286596e421f48ff6185a8fa
Hamnah <20210841@lamolina.edu.pe>
2023-09-21 21:06:24 -0500

    cursos_deportivos_y_culturales

---
+++ Horario por grupos.pl
@@ -0,0 +1,12 @@
+% Horario por grupos:
+grupo(A,[[Lunes,8am-10am],[Martes,11am-1pm]]).
+grupo(B,[[Lunes,11am-1pm],[Martes,8am-10am]]).
+grupo(C,[[Lunes,10am-11am],[Miercoles,11am-1pm],[Viernes,8am-10am]]).
+grupo(D,[]).
+grupo(E,[]).
+grupo(F,[]).
+grupo(G,[]).
+grupo(H,[]).
+grupo(I,[]).
+grupo(J,[]).
+grupo(K,[]).
---
+++ cursos culturales.pl
@@ -0,0 +1,74 @@
+% CURSOS CULTURALES Y DEPORTIVOS
+% Cursos culturales
+% curso(nombre, grupo, aula,prof, creditos,horario)
+
+curso(artes_Plasticas_Creatividad,i, 8,jose_Santos,1,sab_08:00-10:00)
+curso(artes_Plasticas_Creatividad,[i,j], 8,jose_Santos,1,sab_10:00-12:00)
+
+curso(banda_unalm,c,barraca,pedro_montemayor,1,mie_11:00-13:00)
+curso(banda_unalm,f,barraca,pedro_montemayor,1,mie_15:00-17:00)
+curso(banda_unalm,e,barraca,pedro_montemayor,1,jue_11:00-13:00)
+
+curso(capoeira,ju,ambi2,almeri_Machado,1,jue_14:00-16:00)
+curso(capoeira,h ,ambi2,almeri_Machado,1,jue_16:00-18:00)
+
+curso(danza_de_Costa,z,teatrin,eulogio_Cerron,1,mar_15:00-17:00)
+
+curso(danzas_Folcloricas,i,teatrin,patricia_Romero,1,sab_09:00-11:00)
+curso(danzas_Folcloricas,j,teatrin,patricia_Romero,1,sab_11:00-13:00)
+
+curso(dibujo_y_Pintura,e,6,sara_Arauco,1,jue_11:00-13:00)	
+curso(dibujo_y_Pintura,z,6,sara_Arauco,1,jue_13:00-15:00)	
+
+curso(fotografia,[g,f],43,oscar_Pinto,1,mie_14:00-16:00)
+curso(fotografia,f,43,oscar_Pinto,1,lun_14:00-16:00)
+curso(fotografia,c,43,oscar_Pinto,1,mie_11:00-13:00)
+
+curso(guitarra,i,teatrin,gustavo_Yataco,sab_08:00-10:00)
+
+curso(marinera_Nortena,ju,teatrin,eulogio_Cerron,jue_14:00-16:00)	
+
+curso(musica_y_Canto_Coral,[i,j],teatrin,gustavo_Yataco,sab_10:00-12:00)
+
+% Cursos de deporte
+
+curso(aerobicos,h,8,jose_santos,1,lun_16:00-18:00)
+curso(aerobicos,f,8,jose_santos,1,mie_15:00-17:00)
+
+curso(banda unalm,c,barraca,pedro_montemayor,1)
+curso(banda unalm,f,barraca,pedro_montemayor,1)
+curso(banda unalm,e,barraca,pedro_montemayor,1)
+
+curso(aikido,h,dojo,percy_rojas,1,Lun_17:00-19:00)
+
+curso(ajedrez,e,s_ajed,federico_Moran,1,mie_08:00-10:00)
+curso(ajedrez,e,s_ajed,federico_Moran,1,jue_11:00-13:00)
+
+curso(beisbol,g,c_f,francisco_Garcia_Naranjo,1,mar_14:00-16:00)
+
+curso(basquet,e,l_b,henry_Viru,1,jue_11:00-13:00)
+
+curso(ciclismo,f,puerta2,aldo_Castellanos,1,mie_15:00-17:00)
+
+curso(futbol,c,estadio,robi_Arcila,vie_08:00-10:00)
+
+curso(futsal_femenino,d,loza5,robi_Arcila,1,jue_08:00-10:00)
+
+curso(handball,e,lozas,rafael_Pun,1,jue_11:00-13:00)
+
+curso(judo,g,ambi2,luis_Mieres,1,vie_14:00-16:00)
+
+curso(karate,f,ambi2,richard_Valdiviezo,1,mie_15:00-17:00)
+curso(karate,g,ambi2,richard_Valdiviezo,1,vie_14:00-16:00)
+
+curso(natacion,a,piscina,paul_Zegarra,1,mar_11:00-13:00)
+
+curso(taekwondo,d ,barraca1,marco_Chavez,1,jue_08:00-10:00)
+curso(taekwondo,ju,barraca1,marco_Chavez,1,jue_14:00-16:00)
+
+curso(tenis_de_mesa,d ,s_t,javier_Berrospi,1,jue_08:00-10:00)
+curso(tenis_de_mesa,ju,s_t,javier_Berrospi,1,jue_14:00-16:00)
+
+curso(tiro_con_Arco,a,estadio,desconocido,1,a,mar_11:00-13:00)
+
+curso(voley,d,l_v,enrique_Larrain,1,d,jue_08:00-10:00)
