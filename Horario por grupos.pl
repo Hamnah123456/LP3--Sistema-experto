6d2e68a13038f0d42cf5cff27699613788578d8c
Hamnah <20210841@lamolina.edu.pe>
2023-09-21 21:10:15 -0500

    horario_por_grupos

--- Horario por grupos.pl
+++ Horario por grupos.pl
@@ -1,12 +1,14 @@
 % Horario por grupos:
-grupo(A,[[Lunes,8am-10am],[Martes,11am-1pm]]).
-grupo(B,[[Lunes,11am-1pm],[Martes,8am-10am]]).
-grupo(C,[[Lunes,10am-11am],[Miercoles,11am-1pm],[Viernes,8am-10am]]).
-grupo(D,[]).
-grupo(E,[]).
-grupo(F,[]).
-grupo(G,[]).
-grupo(H,[]).
-grupo(I,[]).
-grupo(J,[]).
-grupo(K,[]).
+
+horario(A,[[lunes,8am-10am],[martes,11am-1pm],[miercoles,10am-11am]]).
+horario(B,[[lunes,11am-1pm],[Martes,8am-10am],[jueves,10am-11am]]).
+horario(C,[[lunes,10am-11am],[miercoles,11am-1pm],[viernes,8am-10am]]).
+horario(D,[[martes,10am-11am],[jueves,8am-10am],[viernes,11am-1pm]]).
+horario(E,[[miercoles,8am-10am],[jueves,11am-1pm],[viernes,10am-11am]]).
+horario(F,[[lunes,2pm-4pm],[martes,4pm-5pm],[miercoles,3pm-5pm]]).
+horario(G,[[martes,2pm-4pm],[miercoles,2pm-3pm],[viernes,2pm-4pm]]).
+horario(H,[[lunes,4pm-7pm],[jueves,4pm-6pm]]).
+horario(I,[[miercoles,5pm-7pm],[sabado,8am-11am]]).
+horario(J,[[jueves,6pm-8pm],[sabado,11am-2pm]]).
+horario(K,[[martes,5pm-7pm],[viernes,4pm-7pm]]).
+horario(JU,[jueves,2pm-4pm])
