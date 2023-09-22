beac0099120f9f56c738fd50721d45b81e5e592a
marycarmenromero <109095903+marycarmenromero@users.noreply.github.com>
2023-09-22 09:31:43 -0500

    Add files via upload

---
+++ 5ciclo.pl
@@ -0,0 +1,7 @@
+% Cursos del 5to ciclo
+
+curso(Analisis_de_Regresion,K,[LES1,86],Samuel_Huamani,4).
+curso(Calculo_de_Probabilidades,A,86,Clodomiro_Miranda,4).
+curso(Diseños_Experimentales_1,H,[91,LES1],Luz_Bullon,4).
+curso(Estrategias_de_Muestreo,F,[86,LES2],[Celso_Gonzales,Denise_Chalan],4).
+curso(Lenguaje_de_Programacion_3,C,LES3,[Frida_Coaquira,Ivan_Soto],4).
