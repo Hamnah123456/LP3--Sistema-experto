b381889445468f8891ebbba6bf047946366185c0
marycarmenromero <109095903+marycarmenromero@users.noreply.github.com>
2023-09-22 11:12:46 -0500

    Add files via upload

---
+++ 7ciclo.pl
@@ -0,0 +1,7 @@
+% Cursos del 7tmo ciclo
+
+curso(sistemas_de_informacion_gerencial,h,[[grimaldo_febres],[beatriz_montaño]],4,[[lun_16:00-19:00],[jue_16:00-18:00]]).
+curso(modelos_lineales_1,b,[[87],[les3]],luz_bullon,4,[[mar_8:00-10:00],[jue_10:00-11:00],[lun_11:00-13:00]]).
+curso(estadistica_bayesiana,i,les2,[[carlos_lopez],[mauricio_maguiña]],4,[[sab_8:00-11:00],[mie_17:00-19:00]]).
+curso(estadistica_computacional,e,frida_coaquira,4,[[mie_8:00-10:00],[vie_10:00-11:00],[jue_11:00-13:00]]).
+curso(marketing,g,dennisse_paredes,3,[[mar_14:00-16:00],[vie_14:00-16:00]]).
