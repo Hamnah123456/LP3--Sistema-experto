511f0b5031bf2f7da6f07b8e1a7bd67e227d2872
marycarmenromero <109095903+marycarmenromero@users.noreply.github.com>
2023-09-22 09:58:59 -0500

    5ciclo.pl

--- 5ciclo.pl
+++ 5ciclo.pl
@@ -1,7 +1,7 @@
 % Cursos del 5to ciclo
 
-curso(analisis_de_regresion,K,[les1,86],samuel_huamani,4).
-curso(calculo_de_probabilidades,A,86,clodomiro_miranda,4).
-curso(diseños_Experimentales_1,H,[91,les1],luz_bullon,4).
-curso(estrategias_de_muestreo,F,[86,les2],[celso_gonzales,denise_chalan],4).
-curso(lenguaje_de_programacion_3,C,les3,[frida_coaquira,ivan_soto],4).
+curso(analisis_de_regresion,k,[les1,86],samuel_huamani,4,[vie_16:00-19:00 ,mar_17:00-19:00]).
+curso(calculo_de_probabilidades,a,86,clodomiro_miranda,4,[lun_8:00-10:00,mie_10:00-11:00,mar_11:00-13:00]).
+curso(diseños_Experimentales_1,h,[91,les1],luz_bullon,4,[lun_16:00-18:00,jue_15:00-16:00,jue_16:00-18:00]).
+curso(estrategias_de_muestreo,f,[86,les2],[celso_gonzales,denise_chalan],4,[mar_16:00-17:00,mie_15:00-17:00,lun_14:00-16:00]).
+curso(lenguaje_de_programacion_3,c,les3,[frida_coaquira,ivan_soto],4,[lun_10:00-11:00,vie_8:00-10:00,mie_11:00-13:00]).
