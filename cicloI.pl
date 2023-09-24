171309ce52a3ea8a1a2c23770d819cf86e391953
smlucky12 <107592025+smlucky12@users.noreply.github.com>
2023-09-23 20:05:31 -0500

    Add files via upload

---
+++ cicloI.pl
@@ -0,0 +1,130 @@
+% CURSOS DEL PRIMER CICLO
+% Cursos culturales
+% curso(nombre, grupo, aula,prof, creditos,horario)
+
+
+
+curso(introduccion_a_la_ciencia_de _datos,k2,91,jesus_gamboa_u,3,vie_16:00-18:00).
+curso(introduccion_a_la_ciencia_de _datos,g2,les3,jesus_gamboa_u,3,vie_14:00-16:00).
+curso(introduccion_a_la_ciencia_de _datos,z*,les3,jesus_gamboa_u,3,mie_16:00-18:00).
+
+curso(quimica_general, a2, 54, diego_suarez_r, 3, lun_08:00-10:00).
+curso(quimica_general, a*, q1, jhonny_valverde_f, 3, mar_11:00-13:00).
+curso(quimica_general, a*, q4, fernando_grandez_a, 3, mar_11:00-13:00).
+curso(quimica_general, c*, 54, flora_huaman_p, 3, mie_11:00-13:00).
+curso(quimica_general, c2, q1, eduardo_rodriguez_c, 3, vie_08:00-10:00).
+curso(quimica_general, c2, q3, jacqueline_dioses_m, 3, vie_08:00-10:00).
+curso(quimica_general, a*, 54, elvito_villegas_s, 3, mar_11:00-13:00).
+curso(quimica_general, a2, q1, amelia_arias_d, 3, lun_08:00-10:00).
+curso(quimica_general, a2, q2, mariella_cortez_c, 3, lun_08:00-10:00).
+curso(quimica_general, a*, 55, maria_alegria_a, 3, mar_11:00-13:00).
+curso(quimica_general, a2, q3, fernando_grandez_a, 3, lun_08:00-10:00).
+curso(quimica_general, a2, q4, eduardo_rodriguez_c, 3, lun_08:00-10:00).
+curso(quimica_general, b*, 66, mary_cesare_c, 3, lun_11:00-13:00).
+curso(quimica_general, b2, q1, jhonny_valverde_f, 3, mar_08:00-10:00).
+curso(quimica_general, b2, q2, mariella_cortez_c, 3, mar_08:00-10:00).
+curso(quimica_general, b*, 67, eduardo_rodriguez_c, 3, lun_11:00-13:00).
+curso(quimica_general, b2, q3, fernando_grandez_a, 3, mar_08:00-10:00).
+curso(quimica_general, b2, q4, jacqueline_dioses_m, 3, mar_08:00-10:00).
+curso(quimica_general, e*, 54, flora_huaman_p, 3, jue_11:00-13:00).
+curso(quimica_general, e2, q1, amelia_arias_d, 3, mie_08:00-10:00).
+curso(quimica_general, e2, q2, eduardo_rodriguez_c, 3, mie_08:00-10:00).
+curso(quimica_general, e*, 55, tatiana_rojas_a, 3, jue_11:00-13:00).
+curso(quimica_general, e2, q3, fernando_grandez_a, 3, mie_08:00-10:00).
+curso(quimica_general, e2, q4, jacqueline_dioses_m, 3, mie_08:00-10:00).
+curso(quimica_general, d*, 54, clara_figueroa_c, 3, vie_11:00-13:00).
+curso(quimica_general, d1, q1, amelia_arias_d, 3, jue_08:00-10:00).
+curso(quimica_general, d1, q2, mary_cesare_c, 3, jue_08:00-10:00).
+curso(quimica_general, d*, 55, mary_cesare_c, 3, vie_11:00-13:00).
+curso(quimica_general, d1, q3, fernando_grandez_a, 3, jue_08:00-10:00).
+curso(quimica_general, d1, q7, jacqueline_dioses_m, 3, jue_08:00-10:00).
+curso(quimica_general, f*, 67, mary_cesare_c, 3, lun_14:00-16:00).
+curso(quimica_general, f2, q1, flora_huaman_p, 3, mie_14:00-16:00).
+curso(quimica_general, c*, q3, jhonny_valverde_f, 3, mie_11:00-13:00).
+curso(quimica_general, f*, 68, jhonny_valverde_f, 3, lun_14:00-16:00).
+curso(quimica_general, f2, q3, jhonny_valverde_f, 3, mie_14:00-16:00).
+curso(quimica_general, f2, q7, eduardo_rodriguez_c, 3, mie_14:00-16:00).
+curso(quimica_general, g*, 54, clara_figueroa_c, 3, mar_14:00-16:00).
+curso(quimica_general, g2, q1, amelia_arias_d, 3, vie_14:00-16:00).
+curso(quimica_general, g2, q7, fernando_grandez_a, 3, vie_14:00-16:00).
+curso(quimica_general, g*, 55, maria_alegria_a, 3, mar_14:00-16:00).
+curso(quimica_general, g2, q3, miki_gonzales_u, 3, vie_14:00-16:00).
+curso(quimica_general, e*, q3, amelia_arias_d, 3, jue_11:00-13:00).
+
+curso(analisis_matematico_i, b2, 54, ivan_saavedra_m., 3, mar_08:00-10:00).
+curso(analisis_matematico_i, b*, 54, ivan_saavedra_m., 3, lun_11:00-13:00).
+curso(analisis_matematico_i, b2, 55, carmen_monzon_m., 3, mar_08:00-10:00).
+curso(analisis_matematico_i, b*, 55, jose_alcalde_s., 3, lun_11:00-13:00).
+curso(analisis_matematico_i, b2, 56, rocio_delgado_a., 3, mar_08:00-10:00).
+curso(analisis_matematico_i, b*, 56, rocio_delgado_a., 3, lun_11:00-13:00).
+curso(analisis_matematico_i, b2, 57, ana_zela_a., 3, mar_08:00-10:00).
+curso(analisis_matematico_i, b*, 57, ana_zela_a., 3, lun_11:00-13:00).
+curso(analisis_matematico_i, f2, 54, jorge_condena_c., 3, mie_14:00-16:00).
+curso(analisis_matematico_i, f*, 54, jorge_condena_c., 3, lun_14:00-16:00).
+curso(analisis_matematico_i, f2, 55, carmen_monzon_m., 3, mie_14:00-16:00).
+curso(analisis_matematico_i, f*, 55, carmen_monzon_m., 3, lun_14:00-16:00).
+curso(analisis_matematico_i, f2, 56, alfredo_moreno_l., 3, mie_14:00-16:00).
+curso(analisis_matematico_i, f*, 56, alfredo_moreno_l., 3, lun_14:00-16:00).
+curso(analisis_matematico_i, f2, 57, zelideth_perez_t., 3, mie_14:00-16:00).
+curso(analisis_matematico_i, f*, 57, zelideth_perez_t., 3, lun_14:00-16:00).
+curso(analisis_matematico_i, f2, 66, maria_villanueva_p., 3, mie_14:00-16:00).
+curso(analisis_matematico_i, f*, 66, maria_villanueva_p., 3, lun_14:00-16:00).
+curso(analisis_matematico_i, h*, 54, ivan_saavedra_m., 3, jue_16:00-18:00).
+curso(analisis_matematico_i, h2, 54, ivan_saavedra_m., 3, lun_16:00-18:00).
+curso(analisis_matematico_i, h*, 55, dany_nina_h., 3, jue_16:00-18:00).
+curso(analisis_matematico_i, h2, 55, dany_nina_h., 3, lun_16:00-18:00).
+curso(analisis_matematico_i, h*, 56, pedro_suarez_n., 3, jue_16:00-18:00).
+curso(analisis_matematico_i, h2, 56, pedro_suarez_n., 3, lun_16:00-18:00).
+curso(analisis_matematico_i, h*, 57, rocio_delgado_a., 3, jue_16:00-18:00).
+curso(analisis_matematico_i, h2, 57, rocio_delgado_a., 3, lun_16:00-18:00).
+curso(analisis_matematico_i, h*, 66, maria_salazar_d., 3, jue_16:00-18:00).
+curso(analisis_matematico_i, h2, 66, patricia_muñoz_a., 3, lun_16:00-18:00).
+
+curso(administracion_general, f2, 69, elvira_caceres_c., 3, mie_14:00-16:00).
+curso(administracion_general, f*, 69, elvira_caceres_c., 3, lun_14:00-16:00).
+
+curso(sociedad_y_cultura_peruana, d4, 65, jimmy_yepez_a, 2, jue_10:00-11:00).
+curso(sociedad_y_cultura_peruana, e*, 65, jimmy_yepez_a, 2, jue_11:00-13:00).
+curso(sociedad_y_cultura_peruana, d4, 57, humberto_porras_v, 2, jue_10:00-11:00).
+curso(sociedad_y_cultura_peruana, e*, 57, humberto_porras_v, 2, jue_11:00-13:00).
+curso(sociedad_y_cultura_peruana, b4, 68, humberto_porras_v, 2, mar_10:00-11:00).
+curso(sociedad_y_cultura_peruana, a*, 68, humberto_porras_v, 2, mar_11:00-13:00).
+curso(sociedad_y_cultura_peruana, a4, 60, ernesto_maguina_s, 2, lun_10:00-11:00).
+curso(sociedad_y_cultura_peruana, b*, 60, ernesto_maguina_s, 2, lun_11:00-13:00).
+curso(sociedad_y_cultura_peruana, e5, 65, jimmy_yepez_a, 2, mie_10:00-11:00).
+curso(sociedad_y_cultura_peruana, c*, 65, jimmy_yepez_a, 2, mie_11:00-13:00).
+curso(sociedad_y_cultura_peruana, b4, 50, pedro_ccopa_a, 2, mar_10:00-11:00).
+curso(sociedad_y_cultura_peruana, a*, 50, pedro_ccopa_a, 2, mar_11:00-13:00).
+curso(sociedad_y_cultura_peruana, e1, 67, ernesto_maguina_s, 2, mie_08:00-09:00).
+curso(sociedad_y_cultura_peruana, e3, 67, ernesto_maguina_s, 2, mie_09:00-11:00).
+curso(sociedad_y_cultura_peruana, c4, 65, jimmy_yepez_a, 2, vie_10:00-11:00).
+curso(sociedad_y_cultura_peruana, d*, 65, jimmy_yepez_a, 2, vie_11:00-13:00).
+curso(sociedad_y_cultura_peruana, e5, 69, julio_chavez_a, 2, mie_10:00-11:00).
+curso(sociedad_y_cultura_peruana, c*, 69, julio_chavez_a, 2, mie_11:00-13:00).
+curso(sociedad_y_cultura_peruana, h4, 65, jimmy_yepez_a, 2, mie_16:00-17:00).
+curso(sociedad_y_cultura_peruana, h*, 65, jimmy_yepez_a, 2, jue_16:00-18:00).
+curso(sociedad_y_cultura_peruana, h1, 67, ernesto_maguina_s, 2, lun_16:00-17:00).
+curso(sociedad_y_cultura_peruana, z*, 67, ernesto_maguina_s, 2, jue_15:00-17:00).
+curso(sociedad_y_cultura_peruana, c1, 57, julio_chavez_a, 2, vie_08:00-09:00).
+curso(sociedad_y_cultura_peruana, c5, 57, julio_chavez_a, 2, vie_09:00-11:00).
+
+curso(ecologia_general, a, 55, diana_quinteros_c, 3, lun_08:00-11:00).
+curso(ecologia_general, a, 53, percy_zorogastua_c, 3, lun_08:00-11:00).
+curso(ecologia_general, a, 52, javier_arias_c, 3, lun_08:00-11:00).
+curso(ecologia_general, a, 56, fabiola_parra_r, 3, lun_08:00-11:00).
+curso(ecologia_general, g, 54, claudia_caro_v, 3, mar_13:00-14:00).
+curso(ecologia_general, g, 54, claudia_caro_v, 3, vie_14:00-16:00).
+curso(ecologia_general, g, 55, ayling_wetzell_c, 3, mar_13:00-14:00).
+curso(ecologia_general, g, 55, ayling_wetzell_c, 3,vie_14:00-16:00).
+curso(ecologia_general, g, 56, richar_morales_r, 3, mar_13:00-14:00).
+curso(ecologia_general, g, 56, richar_morales_r, 3, vie_14:00-16:00).
+curso(ecologia_general, g, 57, liz_castaneda_c, 3, mar_13:00-14:00).
+curso(ecologia_general, g, 57, liz_castaneda_c, 3, vie_14:00-16:00).
+curso(ecologia_general, g, 66, gladys_tello_c, 3, mar_13:00-14:00).
+curso(ecologia_general, g, 66, gladys_tello_c, 3,vie_14:00-16:00).
+curso(ecologia_general, e, 54, claudia_caro_v, 3, mie_08:00-11:00).
+curso(ecologia_general, e, 55, fabiola_parra_r, 3, mie_08:00-11:00).
+curso(ecologia_general, e, 56, liz_castaneda_c, 3, mie_08:00-11:00).
+curso(ecologia_general, e, 57, richar_morales_r, 3, mie_08:00-11:00).
+curso(ecologia_general, e, 66, gladys_tello_c, 3, mie_08:00-11:00).
+
---
+++ cicloIII.pl
@@ -0,0 +1,73 @@
+% CURSOS DEL TERCER CICLO
+% Cursos culturales
+% curso(nombre, grupo, aula,prof, creditos,horario)
+
+% Teoría
+curso(redaccion_argumentacion, b2, 81, maria_mandujano_r, 2, mar_08:00-10:00).
+curso(redaccion_argumentacion, g2, 83, nehemias_vega_m, 2, vie_14:00-16:00).
+curso(redaccion_argumentacion, k, 83, nehemias_vega_m, 2, vie_16:00-18:00).
+curso(redaccion_argumentacion, f2, 84, lily_cuadros_a, 2, mie_14:00-16:00).
+curso(redaccion_argumentacion, d1, 76, jim_anchante_a, 2, jue_08:00-10:00).
+curso(redaccion_argumentacion, b2, 77, janet_corzo_z, 2, mar_08:00-10:00).
+curso(redaccion_argumentacion, f*, 82, janet_corzo_z, 2, lun_14:00-16:00).
+curso(redaccion_argumentacion, a2, 74, tomas_barriga_b, 2, lun_08:00-10:00).
+curso(redaccion_argumentacion, a*, 77, silvia_valdez_d, 2, mar_11:00-13:00).
+curso(redaccion_argumentacion, g2, 82, lily_cuadros_a, 2, vie_14:00-16:00).
+curso(redaccion_argumentacion, a2, 75, maria_mandujano_r, 2, lun_08:00-10:00).
+curso(redaccion_argumentacion, c*, 77, silvia_valdez_d, 2, mie_11:00-13:00).
+curso(redaccion_argumentacion, f2, 85, edmundo_de_la_sota_d, 2, mie_14:00-16:00).
+curso(redaccion_argumentacion, z, 4P, 6000, edmundo_de_la_sota_d, 2, sab_10:00-12:00).
+curso(redaccion_argumentacion, c2, 75, mario_castillo_h, 2, vie_08:00-10:00).
+curso(redaccion_argumentacion, d*, 75, mario_castillo_h, 2, vie_11:00-13:00).
+curso(redaccion_argumentacion, g*, 80, g840, janet_corzo_z, 2, mar_14:00-16:00).
+
+% Teoría
+curso(algebra_matricial, c, 19, carlos_rodriguez_f, 4, vie_08:00-11:00).
+
+% Práctica
+curso(algebra_matricial, c*, 19, carmen_monzon_m, 4, mie_11:00-13:00).
+
+% Teoría
+curso(matematicas_discretas, d, 17, carlos_rodriguez_f, 4, jue_08:00-11:00).
+
+% Práctica
+curso(matematicas_discretas, d*, 17, ivan_saavedra_m, 4, vie_11:00-13:00).
+
+
+% Teoría
+curso(ingenieria_de_procesos, a, 87, raphael_valencia_c, 4, lun_08:00-11:00).
+
+% Práctica
+curso(ingenieria_de_procesos, a*, 87, raphael_valencia_c, 4, mar_11:00-13:00).
+
+% Teoría
+curso(estadistica_general, g2, 90, samuel_huamani_f, 3, vie_14:00-16:00).
+curso(estadistica_general, b2, 89, aldo_meza_r, 3, mar_08:00-10:00).
+curso(estadistica_general, a2, 89, cesar_menacho_c, 3, lun_08:00-10:00).
+curso(estadistica_general, b2, 90, rolando_salazar_v, 3, mar_08:00-10:00).
+curso(estadistica_general, c*, 89, jorge_chue_g, 3, mie_11:00-13:00).
+curso(estadistica_general, g2, 89, viernes_rosazza_v, 3, vie_14:00-16:00).
+curso(estadistica_general, d1, 89, viernes_rosazza_v, 3, jue_08:00-10:00).
+curso(estadistica_general, d1, 90, juan_orosco_g, 3, jue_08:00-10:00).
+curso(estadistica_general, b2, 91, juan_orosco_g, 3, mar_08:00-10:00).
+curso(estadistica_general, k2, 89, viernes_rosazza_v, 3, vie_16:00-18:00).
+
+% Práctica
+curso(estadistica_general, g*, 90, samuel_huamani_f, 3, mar_14:00-16:00).
+curso(estadistica_general, b*, 89, denise_chalan_l, 3, lun_11:00-13:00).
+curso(estadistica_general, a*, 89, cesar_menacho_c, 3, mar_11:00-13:00).
+curso(estadistica_general, b*, 90, mauricio_maguina_m, 3, lun_11:00-13:00).
+curso(estadistica_general, c2, 89, beatriz_montano_m, 3, vie_08:00-10:00).
+curso(estadistica_general, g*, 89, mauricio_maguina_m, 3, mar_14:00-16:00).
+curso(estadistica_general, d*, 89, beatriz_montano_m, 3, vie_11:00-13:00).
+curso(estadistica_general, d*, 90, denise_chalan_l, 3, vie_11:00-13:00).
+curso(estadistica_general, b*, 91, juan_orosco_g, 3, lun_11:00-13:00).
+curso(estadistica_general, k*, 89, viernes_rosazza_v, 3, mar_16:00-18:00).
+
+
+% Teoría
+curso(lenguaje_programacion_i, c, les3, ana_vargas_p, 4, vie_08:00-11:00).
+
+% Práctica
+curso(lenguaje_programacion_i, c*, les3, ana_vargas_p, 4, mie_11:00-13:00).
+
