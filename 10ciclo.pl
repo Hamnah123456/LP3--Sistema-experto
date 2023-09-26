72bbef1339ce5fefa0317df70cdd3faa1658ad17
marycarmenromero <109095903+marycarmenromero@users.noreply.github.com>
2023-09-25 19:29:16 -0500

    10ciclo.pl

--- 10ciclo.pl
+++ 10ciclo.pl
@@ -1,5 +1,5 @@
 % Cursos del 10cmo ciclo
 
-curso(ciencias_de_datos_2,a,[[87],[les2]],aldo_meza,4,[[lun_8:00-10:00],[mie_10:00-11:00],[mar_11:00-13:00]]).
-curso(seminario_en_estadistica_informatica_2,[[b],[e]],[[87],[86]],[[fernando_rosas],[jesus_salinas]],4,[[lun_11:00-13:00],[jue_11:00-13:00]]).
-curso(tecnologias_emergentes,e,les2,jorge_chue,4,[[mie_8:00-10:00],[vie_10:00-11:00],[jue_11:00-13:00]]).
+curso(ciencias_de_datos_2,a,[87,les2],aldo_meza,4,[lun_8:00-10:00,mie_10:00-11:00,mar_11:00-13:00]).
+curso(seminario_en_estadistica_informatica_2,[b,e],[87,86],[fernando_rosas,jesus_salinas],4,[lun_11:00-13:00,jue_11:00-13:00]).
+curso(tecnologias_emergentes,e,les2,jorge_chue,4,[mie_8:00-10:00,vie_10:00-11:00,jue_11:00-13:00]).
