77f1a03e718c9a10bba5058c68e3bf3be8005b7b
PaolaRomeroSolano <72429928+PaolaRomeroSolano@users.noreply.github.com>
2023-09-26 13:40:37 -0500

    Add files via upload

---
+++ 8ciclo.pl
@@ -0,0 +1,20 @@
+% Cursos_8to_ciclo
+
+curso(seminario_en_estadistica_informatica_1, d2, 72, jesus_gamboa, jue_08:00-10:00).
+curso(seminario_en_estadistica_informatica_1, e, 72, jaime_porras, jue_11:00-13:00).
+
+curso(modelos_lineales_ii, a, 88, cesar_menacho_c, 4, [lun_08:00-10:00,mie_10:00-11:00]).
+curso(modelos_lineales_ii, a, 88, mauricio_maguiña, 4, mar_11:00-13:00).
+
+curso(maquinas_de_aprendizaje, h, les2, carlos_lopez_de_castilla_v, 4, lun_16:00-19:00).
+curso(maquinas_de_aprendizaje, h, les2, carlos_lopez_de_castilla_v, 4, jue_16:00-18:00).
+
+curso(investigacion_de_mercados, i2, 89, viernes_rosazza_v, 4, [mar_08:00-10:00,jue_10:00-11:00]).
+curso(investigacion_de_mercados, i, 89, viernes_rosazza_v, 4, mie_17:00-19:00).
+
+curso(gestion_estrategica_de_datos, z, les1, felipe_de_mendiburu_d, 4, [jue_13:00-16:00,vie_08:00-10:00]).
+curso(gestion_estrategica_de_datos, c2, 45, felipe_de_mendiburu_d, 4, vie_08:00-10:00).
+
+
+curso(estadistica_no_parametrica, b, 86, jaime_porras_c, 4, [mar _08:00-10:00, jue_10:00-11:00]).
+curso(estadistica_no_parametrica, b, les2, jaime_porras_c, 4, lun_11:00-13:00).
