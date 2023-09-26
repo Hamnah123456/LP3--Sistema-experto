4136180159cdbfc45d8e8a6686abdd94dda7ec4d
PaolaRomeroSolano <72429928+PaolaRomeroSolano@users.noreply.github.com>
2023-09-26 18:42:05 -0500

    Add files via upload

---
+++ 9ciclo.pl
@@ -0,0 +1,10 @@
+% Cursos_9to_ciclo
+
+curso(analisis_de_series_de_tiempo, k, [86,les3], rino_sotomayor, [vie_16:00-19:00,mar_17:00-19:00]).
+
+curso(ciencia_de_datos_i, a, [87,les2], aldo_meza, 4, [lun_08:00-10:00,mie_10:00-11:00,mar_11:00-13:00]).
+
+curso(estadistica_espacial, f, les1, felipe_de_mendiburu_d, 4, [mie_15:00-17:00,lun_14:00-16:00]).
+
+curso(gestion_de_proyectos_de_informacion, c, 87, jorge_chue_g, 4, [lun_10:00-11:00,vie_08:00-10:00,mie_11:00-13:00]).
+
